d38d70904ccb94e4b177ad57dda10e09509d4e8b
VortexDev1 <nadiatx123@gmail.com>
2024-11-19 14:04:26 +0200

    Update and rename script.js to JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,46 @@
+function toggleMenu() {
+  const menuLinks = document.getElementById('menu-links');
+  menuLinks.classList.toggle('show');
+}
+
+document.querySelector('.button1').addEventListener('click', function() {
+  window.scrollTo({
+    top: document.querySelector('.mods').offsetTop,
+    behavior: 'smooth'
+  });
+});
+
+document.querySelector('.aboutbtn').addEventListener('click', function() {
+  window.scrollTo({
+    top: document.querySelector('.about').offsetTop,
+    behavior: 'smooth'
+  });
+});
+
+document.querySelector('.downloadbtn').addEventListener('click', function() {
+  window.scrollTo({
+    top: document.querySelector('.mods').offsetTop,
+    behavior: 'smooth'
+  });
+});
+
+document.querySelector('.linksY').addEventListener('click', function() {
+  window.open('https://www.youtube.com', '_blank');
+});
+
+document.querySelector('.linksD').addEventListener('click', function() {
+  window.open('https://www.discord.com', '_blank');
+});
+
+document.querySelectorAll('.button2').forEach(button => {
+  button.addEventListener('click', function() {
+    const filePath = button.getAttribute('data-file');
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.download = filePath.split('/').pop();
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+};
--- script.js
+++
@@ -1,7 +0,0 @@
-// Smooth scroll to the popular mods section
-document.querySelector('.cta-button').addEventListener('click', function() {
-  window.scrollTo({
-    top: document.querySelector('.popular-mods').offsetTop,
-    behavior: 'smooth'
-  });
-});
